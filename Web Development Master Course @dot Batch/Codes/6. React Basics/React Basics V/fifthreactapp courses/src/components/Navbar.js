67d962f1fa9bb8ea82cc93fc30f212a584987998
17ved <vedantthombare792@gmail.com>
2024-04-07 21:07:28 +0530

    first

---
+++ Web Development Master Course @dot Batch/Codes/6. React Basics/React Basics V/fifthreactapp courses/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-bgDark py-4">
+        <h1 className="text-center text-3xl text-white font-bold">
+          {" "}
+          Top Courses{" "}
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
